e0e04e1c2f0be8166b81771d3525571ec791b377
Matthijs Alles <admin@bixie.nl>
2014-08-22 23:02:39 +0200

    v1

---
+++ templates/yoo_capture/html/mod_footer/bps.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_footer
+ * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+<ul class="uk-subnav uk-subnav-line">
+	<li><?php echo $lineone; ?></li>
+	<li><span>Website door <a href="http://www.bixie.nl" target="_blank" title="Bixie"><i class="uk-icon-bixie uk-margin-small-right"></i>Bixie</a></span></li>
+</ul>
